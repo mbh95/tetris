968801eaaa316e00286c51281f66e57adfaaf0a1
mhammond <matthewbh95@gmail.com>
2019-09-25 09:29:41 -0700

    Removed starter code. Changed project name to Tetris.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import printMe from './print';
-
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
-    return element;
-  }
-
-  document.body.appendChild(component());
+console.log("Hello Tetris!");
