4b3775af63f84f173af9d32b1161bb8cd9f4ea2f
Matt Hammond <matthewbh95@gmail.com>
2019-10-02 21:51:17 -0700

    Implemented basic game state and some util functions.

--- src/game/tetrisGame.ts
+++ src/game/tetrisGame.ts
@@ -1,47 +1,102 @@
-import {TetrisActionEvent} from "../events/actionEvent";
-import {Queue} from "../util/queue";
+import {TetrisActionEvent, TetrisActionEventType} from "../events/actionEvent";
+import {PieceGenerator} from "./state/pieceGenerator";
+import {Matrix} from "./state/matrix";
+import {Piece, PiecePrototype} from "./state/piece";
+import {DIR_DOWN, DIR_LEFT, DIR_RIGHT, isPieceOnGround} from "./tetrisUtils";
+import {Position} from "./state/position";
 
-export interface TetrisGameState {
+export interface TetrisGame {
+    readonly matrix: Matrix;
+    readonly fallingPiece: Piece | undefined;
+    readonly heldPiece: PiecePrototype | undefined;
+    readonly queue: PieceGenerator;
 
-    handleActionEvent(e: TetrisActionEvent): void;
+    handleActionEvent(e: TetrisActionEvent): TetrisGame;
 
-    update(dt: number): void;
+    update(dt: number): TetrisGame;
 }
 
-class ActionPrinterGameState implements TetrisGameState {
-    private countdown = 1.0;
+class FallingPieceState implements TetrisGame {
+    readonly matrix: Matrix;
+    readonly fallingPiece: Piece;
+    readonly heldPiece: PiecePrototype | undefined;
+    readonly queue: PieceGenerator;
 
-    handleActionEvent(e: TetrisActionEvent): void {
-        console.log(e);
-    }
+    readonly gravityRate: number;
+    readonly gravityAccumulator: number;
+    readonly lockCountdown: number;
+    readonly canHold: boolean;
 
-    update(dt: number): void {
-        this.countdown -= dt;
-        if (this.countdown < 0) {
-            console.log("tick");
-            this.countdown = 1.0;
+    constructor(matrix: Matrix, fallingPiece: Piece | undefined, heldPiece: PiecePrototype | undefined, queue: PieceGenerator, gravityRate: number, gravityAccumulator = 0.0, lockCountdown = 3.0, canHold = true) {
+        this.matrix = matrix;
+        this.heldPiece = heldPiece;
+        if (fallingPiece === undefined) {
+            const queueNext = queue.next();
+            this.fallingPiece = new Piece(queueNext.pieceProto, 0, matrix.spawnPos);
+            this.queue = queueNext.generator;
+        } else {
+            this.fallingPiece = fallingPiece;
+            this.queue = queue;
         }
-    }
-}
-
-export class TetrisGame {
-
-    private actionBuffer: Queue<TetrisActionEvent> = new Queue();
-    readonly tetrisActionCallback: (e: TetrisActionEvent) => void = (e: TetrisActionEvent) => this.actionBuffer.push(e);
-
-    private state: TetrisGameState = new ActionPrinterGameState();
-
-    update(dt: number): void {
-        this.handleActionEvents();
-        this.state.update(dt);
+        this.gravityRate = gravityRate;
+        this.gravityAccumulator = gravityAccumulator;
+        this.lockCountdown = lockCountdown;
+        this.canHold = canHold;
     }
 
-    handleActionEvents() {
-        while (!this.actionBuffer.isEmpty()) {
-            const e: TetrisActionEvent | undefined = this.actionBuffer.pop();
-            if (e !== undefined) {
-                this.state.handleActionEvent(e);
+    handleActionEvent(e: TetrisActionEvent): TetrisGame {
+        switch (e.type) {
+            case TetrisActionEventType.MOVE_L:
+                return this.movePiece(DIR_LEFT);
+            case TetrisActionEventType.MOVE_R:
+                return this.movePiece(DIR_RIGHT);
+            case TetrisActionEventType.SOFT_DROP:
+                return this.movePiece(DIR_DOWN);
+            case TetrisActionEventType.HOLD:
+                return this.hold();
+        }
+        return this;
+    }
+
+    private movePiece(dPos: Position): TetrisGame {
+        const newPiece: Piece = this.fallingPiece.translated(dPos);
+        if (this.matrix.isPieceValid(newPiece)) {
+            return new FallingPieceState(this.matrix, newPiece, this.heldPiece, this.queue, this.gravityRate, this.gravityAccumulator, this.lockCountdown, this.canHold);
+        }
+        return this;
+    }
+
+    private hold(): TetrisGame {
+        if (this.canHold) {
+            const newHeldPiece: PiecePrototype = this.fallingPiece.piecePrototype;
+            if (this.heldPiece === undefined) {
+                const queueNext = this.queue.next();
+                const newQueue = queueNext.generator;
+                const newPiece: Piece = new Piece(queueNext.pieceProto, 0, this.matrix.spawnPos);
+                return new FallingPieceState(this.matrix, newPiece, newHeldPiece, newQueue, this.gravityRate, this.gravityAccumulator, this.lockCountdown, false);
             }
+            const newPiece: Piece = new Piece(this.heldPiece, 0, this.matrix.spawnPos);
+            return new FallingPieceState(this.matrix, newPiece, newHeldPiece, this.queue, this.gravityRate, this.gravityAccumulator, this.lockCountdown, false);
+        }
+        return this;
+    }
+
+    update(dt: number): TetrisGame {
+        if (isPieceOnGround(this.fallingPiece, this.matrix)) {
+            const newLockCountdown = this.lockCountdown - dt;
+            if (newLockCountdown <= 0) {
+                const lockResult = this.matrix.lockPiece(this.fallingPiece);
+                return new FallingPieceState(lockResult.newMatrix, undefined, this.heldPiece, this.queue, this.gravityRate);
+            }
+            return new FallingPieceState(this.matrix, this.fallingPiece, this.heldPiece, this.queue, this.gravityRate, this.gravityAccumulator, newLockCountdown, this.canHold);
+        } else {
+            let newGravityAccumulator = this.gravityAccumulator + dt * this.gravityRate;
+            let newPiece = this.fallingPiece;
+            while (newGravityAccumulator >= 1.0 && !isPieceOnGround(newPiece, this.matrix)) {
+                newPiece = newPiece.translated(DIR_DOWN);
+                newGravityAccumulator -= 1.0;
+            }
+            return new FallingPieceState(this.matrix, newPiece, this.heldPiece, this.queue, this.gravityRate, newGravityAccumulator, this.lockCountdown, this.canHold);
         }
     }
 }
---
+++ src/game/tetrisUtils.ts
@@ -0,0 +1,36 @@
+import {Piece} from "./state/piece";
+import {Matrix} from "./state/matrix";
+import {Position} from "./state/position";
+import {List} from "immutable";
+
+export const DIR_UP = new Position({row: 1, col: 0});
+export const DIR_DOWN = new Position({row: -1, col: 0});
+export const DIR_LEFT = new Position({row: 0, col: -1});
+export const DIR_RIGHT = new Position({row: 0, col: 1});
+
+export const ALL_DIRECTIONS: List<Position> = List([DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT]);
+
+export function isPieceOnGround(piece: Piece, matrix: Matrix) {
+    return matrix.isPieceValid(piece) && !matrix.isPieceValid(piece.translated(new Position({row: -1, col: 0})));
+}
+
+// TODO: Binary search for ghost piece?
+export function getGhostPiece(piece: Piece, matrix: Matrix): Piece {
+    let curGhost = piece;
+    let candidate = piece.translated(DIR_DOWN);
+    while (matrix.isPieceValid(candidate)) {
+        curGhost = candidate;
+        candidate = piece.translated(DIR_DOWN);
+    }
+    return curGhost;
+}
+
+export function canPieceMove(piece: Piece, matrix: Matrix): boolean {
+    return !ALL_DIRECTIONS
+        .filter(dir => matrix.isPieceValid(piece.translated(dir))) // Get all directions piece can move.
+        .isEmpty(); // If list is empty then piece can't move, so negate to find if it can.
+}
+
+export function isAllClear(matrix: Matrix): boolean {
+    return matrix.blocks.isEmpty();
+}
